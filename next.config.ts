fb54c62b1492bd0aa0f8b1b7da1be5f86d796255
Brooke Dixon <bdixon@gmail.com>
2024-10-11 21:13:06 -0400

    Add next.config.ts for Next.js config and enable synthetic default imports in tsconfig.json for better module support

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import BuilderDevTools from "@builder.io/dev-tools/next";
+
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
@@ -7,12 +7,12 @@ import BuilderDevTools from "@builder.io/dev-tools/next";
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = BuilderDevTools()({
+const config = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
     domains: ["img.clerk.com"],
   },
-});
-
+};
+  
 export default config;
