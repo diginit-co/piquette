578eb18a7b0cf74b1ec0287a8bbcf628ada1b6f6
Brooke Dixon <bdixon@gmail.com>
2024-10-14 10:50:05 -0400

    build test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,12 +7,12 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-        domains: ['img.clerk.com']
-    }
-};
+const config = BuilderDevTools()({
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    domains: ["img.clerk.com"],
+  },
+});
 
 export default config;
