8861d7f348a4396098180a0592806035e7fc7a27
Brooke Dixon <bdixon@gmail.com>
2024-10-29 09:11:35 -0400

    Add Profiles, Organizations, and Documents tables to the schema; enhance the database structure for user management and document handling

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -179,6 +179,32 @@ export const pins = createTable(
  * 
  */
 
+export const profiles = createTable(
+  "profile",
+  {
+    id: serial("id").primaryKey(),
+    cuid: varchar("cuid", { length: 256 }).notNull(),
+    uuid: varchar("uuid").default(sql`gen_random_uuid()`).notNull(),
+    token: varchar("token", { length: 32 }),
+    user: varchar("user", { length: 256 }).notNull(),
+    provider: varchar("provider", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }),
+    description: text("description"),
+    type: varchar("user", { length: 16 }),
+    avatar: varchar("avatar", { length: 256 }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    createdBy: varchar("created_by", { length: 256 }).notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+    updatedBy: varchar("updated_by", { length: 256 }).notNull(),
+    archivedAt: timestamp("archived_at", { withTimezone: true }),
+    archivedBy: varchar("archived_by", { length: 256 }),
+  },
+);
+
 export const businesses = createTable(
   "business",
   {
@@ -191,6 +217,53 @@ export const businesses = createTable(
     url: varchar("url", { length: 256 }),
     industry: varchar("industry", { length: 256 }),
     description: text("description"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    createdBy: varchar("created_by", { length: 256 }).notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+    updatedBy: varchar("updated_by", { length: 256 }).notNull(),
+    archivedAt: timestamp("archived_at", { withTimezone: true }),
+    archivedBy: varchar("archived_by", { length: 256 }),
+  },
+);
+
+export const organizations = createTable(
+  "organization",
+  {
+    id: serial("id").primaryKey(),
+    cuid: varchar("cuid", { length: 256 }).notNull(),
+    uuid: varchar("uuid").default(sql`gen_random_uuid()`).notNull(),
+    token: varchar("token", { length: 32 }),
+    owner: varchar("owner", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    description: text("description"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    createdBy: varchar("created_by", { length: 256 }).notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+    updatedBy: varchar("updated_by", { length: 256 }).notNull(),
+    archivedAt: timestamp("archived_at", { withTimezone: true }),
+    archivedBy: varchar("archived_by", { length: 256 }),
+  },
+);
+
+export const documents = createTable(
+  "document",
+  {
+    id: serial("id").primaryKey(),
+    cuid: varchar("cuid", { length: 256 }).notNull(),
+    token: varchar("token", { length: 32 }),
+    owner: varchar("owner", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    description: text("description"),
+    url: varchar("url", { length: 256 }),
+    type: varchar("type", { length: 256 }),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
